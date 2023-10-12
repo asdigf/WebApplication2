5064731d72d3e41901191380b56bd83059fdfc29
ACER <ACER@LAPTOP-SPIH0C4B>
2023-10-12 08:43:32 +0700

    Add project files.

---
+++ WebApplication2/Scripts/Javanavbar.js
@@ -0,0 +1,10 @@
+﻿window.addEventListener('scroll', function () {
+    var secondtop = document.querySelector('#second-top');
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollPosition >= 100) { // Điều chỉnh giá trị này để xác định vị trí cuộn tới
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
